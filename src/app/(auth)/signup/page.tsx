4287890034b58529fc53d2873cd2d5c4b4c5cc62
lifeofrifai <muhammadnurifai@gmail.com>
2024-05-06 22:03:53 +0700

    auth folder

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const SignUpPage: NextPage = () => {
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            {/* Your sign in form goes here */}
+        </div>
+    );
+};
+
+export default SignUpPage;
